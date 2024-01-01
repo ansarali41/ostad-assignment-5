8611bda288cedebae01597130ddd8db098cf68e0
Ansar Ali <ansar@amharctech.com>
2024-01-01 23:30:07 +0600

    first commit

---
+++ margeLinkedList.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+const mergeTwoLists = function (list1, list2) {
+    if (!list1 || !list2) return list1 || list2;
+    if (list1.val < list2.val) {
+        return new ListNode(list1.val, mergeTwoLists(list1.next, list2));
+    }
+    return new ListNode(list2.val, mergeTwoLists(list1, list2.next));
+};
+
+// the time complexity is O(n+m)
+// the space complexity is O(n+m).
---
+++ reverseLinkedList.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+const reverseList = function (head) {
+    if (!head || !head.next) return head;
+    reversedPart = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return reversedPart;
+};
+
+// the time complexity is O(n)
+// the space complexity is O(1).
